4c5f0d4ed981f4b01e97f8589350da46d938a957
Erik Hinton <twotonmary@gmail.com>
2020-06-09 16:28:02 -0400

    new directions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  function throttle(callback, wait, immediate = false) {
+function throttle(callback, wait, immediate = false) {
     let timeout = null
     let initialCall = true
     return function() {
@@ -19,18 +19,22 @@
 }
 
 let clicks = 0;
-document.addEventListener("click", () => {
+document.querySelector(".screen").addEventListener("click", () => {
   clicks++
 })
 window.webgazer.setRegression("ridge"); //set a regression module
 
 let averageWindow = []
+let last = null
+let current = null
+let lock = false
 
 window.webgazer.setGazeListener(throttle(function(data, elapsedTime) {
     if (data == null) {
         return;
     }
     if (clicks < 20) return
+
     averageWindow.unshift({x: data.x, y: data.y})
     averageWindow = averageWindow.slice(0,10)
     const [xsum, ysum] = averageWindow.reduce(([xp,yp],{x, y}) => {
@@ -38,22 +42,43 @@ window.webgazer.setGazeListener(throttle(function(data, elapsedTime) {
     }, [0,0])
     var xprediction = xsum/averageWindow.length; //these x coordinates are relative to the viewport
     var yprediction = ysum/averageWindow.length; //these y coordinates are relative to the viewport
+
     const dot = document.querySelector(".dot")
     dot.style.left = xprediction + "px"
     dot.style.top = yprediction + "px"
+
     const pctRight = xprediction / window.innerWidth
     const pctBottom = yprediction / window.innerHeight
-    Array.from(document.querySelectorAll(".frame")).forEach(div => {
-      div.style.display = "none"
-    })
-    if (pctRight < 0.33 && pctBottom > 0.25 && pctBottom < 0.75){
-        document.querySelector(".left").style.display = "block"
-    } else if (pctRight > 0.66 && pctBottom > 0.25 && pctBottom < 0.75){
-        document.querySelector(".right").style.display = "block"
-    } else if (pctBottom <= 0.25) {
-        document.querySelector(".top").style.display = "block"
-    } else if (pctBottom >= 0.75) {
-        document.querySelector(".bottom").style.display = "block"
+
+    if (pctRight < 0.4 && pctBottom < 0.4){
+        current = "ul"
+    } else if (pctRight > 0.6 && pctBottom < 0.4){
+        current = "ur"
+    } else if (pctRight < 0.4 && pctBottom > 0.6) {
+        current = "ll"
+    } else if (pctRight > 0.6 && pctBottom > 0.6){
+        current = "lr"
+    } else {
+        current = "default"
     }
+    
+    if(lock) return
+    if(current === last) {
+        last = current
+    } else {
+        lock = true
+        setTimeout(() => {
+            lock = false
+        }, 1000)
+        Array.from(document.querySelectorAll("iframe")).forEach(div => {
+            div.style.opacity = "0"
+        })
+        Array.from(document.querySelectorAll("." + current)).forEach(div => {
+            div.style.opacity = "1"
+        })
+        last = current
+    }
+
+    
 
 })).begin();
